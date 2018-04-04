055fd11b2f315c8a01e621fef6557a47bd050e88
Duanpham <duanphamhvbc@gmail.com>
2018-04-04 22:12:40 +0700

    Helloworld

--- NoGunny/HelloWorld.java
+++ NoGunny/HelloWorld.java
@@ -3,12 +3,20 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication4;
+package helloworld;
 
 /**
  *
  * @author duanp
  */
 public class HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println(" Hello World!!");
+    }
     
 }
