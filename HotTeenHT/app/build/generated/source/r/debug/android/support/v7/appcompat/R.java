1c50e6ff419cb62d6f5e1790e6ad0009c2aeb7f3
Lai Khang Duy <34293904+laiduy98@users.noreply.github.com>
2018-05-11 10:38:29 +0700

    Update timer

--- HotTeenHT/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
+++ HotTeenHT/app/build/generated/source/r/debug/android/support/v7/appcompat/R.java
@@ -731,16 +731,16 @@ public final class R {
         public static final int abc_search_view = 0x7f0a0018;
         public static final int abc_select_dialog_material = 0x7f0a0019;
         public static final int abc_tooltip = 0x7f0a001a;
-        public static final int notification_action = 0x7f0a0032;
-        public static final int notification_action_tombstone = 0x7f0a0033;
-        public static final int notification_template_custom_big = 0x7f0a003a;
-        public static final int notification_template_icon_group = 0x7f0a003b;
-        public static final int notification_template_part_chronometer = 0x7f0a003f;
-        public static final int notification_template_part_time = 0x7f0a0040;
-        public static final int select_dialog_item_material = 0x7f0a0041;
-        public static final int select_dialog_multichoice_material = 0x7f0a0042;
-        public static final int select_dialog_singlechoice_material = 0x7f0a0043;
-        public static final int support_simple_spinner_dropdown_item = 0x7f0a0045;
+        public static final int notification_action = 0x7f0a0033;
+        public static final int notification_action_tombstone = 0x7f0a0034;
+        public static final int notification_template_custom_big = 0x7f0a003b;
+        public static final int notification_template_icon_group = 0x7f0a003c;
+        public static final int notification_template_part_chronometer = 0x7f0a0040;
+        public static final int notification_template_part_time = 0x7f0a0041;
+        public static final int select_dialog_item_material = 0x7f0a0042;
+        public static final int select_dialog_multichoice_material = 0x7f0a0043;
+        public static final int select_dialog_singlechoice_material = 0x7f0a0044;
+        public static final int support_simple_spinner_dropdown_item = 0x7f0a0046;
     }
     public static final class string {
         public static final int abc_action_bar_home_description = 0x7f0d0000;
--- HotTeenHT/app/src/main/java/com/example/minhduc/fitnessapp/Countdown.java
+++ HotTeenHT/app/src/main/java/com/example/minhduc/fitnessapp/Countdown.java
@@ -1,13 +1,82 @@
 package com.example.minhduc.fitnessapp;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import database.DbHelper;
+import database.DbSchema;
 
 public class Countdown extends AppCompatActivity {
+
+    private CountDownTimer Timer;
+    private TextView viewnumber;
+    private TextView activityname;
+    private DbHelper dbHelper;
+    private SQLiteDatabase db;
+    private ArrayList<Integer> exerciseReps;
+    private ArrayList<String> exerciseNames;
+    private int workoutId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_countdown);
+        viewnumber = (TextView) findViewById(R.id.viewnumber);
+        activityname = (TextView) findViewById(R.id.activityname);
+
+        dbHelper = new DbHelper(this);
+        db = dbHelper.getReadableDatabase();
+        Cursor cursor = getExercisesFromWorkoutCursor(workoutId);
+        int exerciseId;
+        if (cursor.moveToFirst()) {
+            do {
+                exerciseId = cursor.getInt(cursor.getColumnIndex(DbSchema.RelationshipTable.Cols.EXERCISEID));
+                exerciseNames.add(dbHelper.getExerciseName(exerciseId));
+                exerciseReps.add(cursor.getInt(cursor.getColumnIndex(DbSchema.RelationshipTable.Cols.REPS)));
+            } while (cursor.moveToNext());
+        }
+        cursor.close();
+
+        Timer = new CountDownTimer(6000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+                viewnumber.setText((String.valueOf(millisUntilFinished/1000)));
+                activityname.setText("Ready ?");
+            }
+
+            @Override
+            public void onFinish() {
+                for (int a = dbHelper.getRounds(workoutId); a>=0; a--){
+                    Timer = new CountDownTimer(10000,1000) {
+                        @Override
+                        public void onTick(long millisUntilFinished) {
+                            viewnumber.setText(String.valueOf(millisUntilFinished/1000));
+                            activityname.setText("Test");
+                        }
+
+                        @Override
+                        public void onFinish() {
+
+                        }
+                    }.start();
+                }
+            }
+        }.start();
+
+    }
+    public Cursor getExercisesFromWorkoutCursor(int workoutId) {
+        Cursor cursor = db.query(DbSchema.RelationshipTable.NAME,
+                new String[]{DbSchema.RelationshipTable.Cols.EXERCISEID, DbSchema.RelationshipTable.Cols.REPS},
+                DbSchema.RelationshipTable.Cols.WORKOUTID + " = ?",
+                new String[] {Integer.toString(workoutId)},
+                null,null,null);
+        return cursor;
     }
 }
