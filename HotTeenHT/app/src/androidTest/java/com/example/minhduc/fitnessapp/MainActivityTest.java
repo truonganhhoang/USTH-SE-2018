2a2e1db1ca46d75152f7fe327523f339912fad89
minhduc0711 <megaman0711@gmail.com>
2018-05-14 11:08:34 +0700

    Added new quizzes screeenshots & Minor changes to test codes

--- HotTeenHT/app/src/androidTest/java/com/example/minhduc/fitnessapp/MainActivityTest.java
+++ HotTeenHT/app/src/androidTest/java/com/example/minhduc/fitnessapp/MainActivityTest.java
@@ -39,35 +39,37 @@ public class MainActivityTest {
 
     @Test
     public void testListViewBehavior() {
+        String name1 = "Arms";
+        String name2 = "Legs";
         // Click the lets go button
         onView(withId(R.id.customWorkoutButton)).perform(click());
         // Click the add button
         onView(withId(R.id.addWorkoutButton)).perform(click());
         // Type the name
-        onView(allOf(withClassName(endsWith("EditText")), withText(is("")))).perform(replaceText("a"));
+        onView(allOf(withClassName(endsWith("EditText")), withText(is("")))).perform(replaceText(name1));
         // Save the exercise
         onView(withId(R.id.confirmWorkoutNameButton)).perform((click()));
         // Click finish
         onView(withId(R.id.buttonFinish)).perform(click());
         // Check if its appear on menu
-        onView(withText("a")).check(ViewAssertions.matches(isDisplayed()));
-        // Hold the "a" workout line
-        onView(withText("a")).perform(longClick());
+        onView(withText(name1)).check(ViewAssertions.matches(isDisplayed()));
+        // Hold the name1 workout line
+        onView(withText(name1)).perform(longClick());
         // Click Rename
         onView(withText("Rename")).perform(click());
         // Rename the workout
-        onView(allOf(withClassName(endsWith("EditText")), withText(is("")))).perform(replaceText("b"));
+        onView(allOf(withClassName(endsWith("EditText")), withText(is("")))).perform(replaceText(name2));
         // Click OK
         onView(withText("OK")).perform(click());
         // Check if its rename it or not
-        onView(withText("b")).check(ViewAssertions.matches(isDisplayed()));
-        // Hold the "UI TEST" workout line
-        onView(withText("b")).perform(longClick());
+        onView(withText(name2)).check(ViewAssertions.matches(isDisplayed()));
+        // Hold the name2 workout line
+        onView(withText(name2)).perform(longClick());
         // Click Delete
         onView(withText("Delete")).perform(click());
-        // click Ok
+        // Click Ok
         onView(withText("OK")).perform(click());
-        // check if its not appear on menu
-        onView(withText("b")).check(ViewAssertions.doesNotExist());
+        // Check if its not appear on menu
+        onView(withText(name2)).check(ViewAssertions.doesNotExist());
     }
 }
