f84b170977d87665bf793f461f360a84c6e494ab
BachNgX <bach.jax@gmail.com>
2018-05-14 11:15:34 +0700

    debug and add start game feature

---
+++ Protoss/Bird/app/src/main/java/com/example/lugian/bird/StartActivity.java
@@ -0,0 +1,18 @@
+package com.example.lugian.bird;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class StartActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start);
+    }
+    public void startGame(View view){
+        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+    }
+}
